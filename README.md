507dd17ef239ced6c81ada1bf56926a80c0939e9
Erika Pauwels <erika.pauwels@gmail.com>
2020-04-22 10:55:44 +0200

    Initial configuration of a LOD platform exposing a SPARQL endpoint

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mu-project
+# app-centrale-vindplaats
 
 Bootstrap a mu.semte.ch microservices environment in three easy steps.
 
---
+++ config/authorization/config.ex
@@ -0,0 +1,52 @@
+alias Acl.Accessibility.Always, as: AlwaysAccessible
+alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
+alias Acl.GraphSpec, as: GraphSpec
+alias Acl.GroupSpec, as: GroupSpec
+
+defmodule Acl.UserGroups.Config do
+  def user_groups do
+    [
+      # // PUBLIC
+      %GroupSpec{
+        name: "public",
+        useage: [:read],
+        access: %AlwaysAccessible{},
+        graphs: [ %GraphSpec{
+                    graph: "http://mu.semte.ch/application",
+                    constraint: %ResourceConstraint{
+                      resource_types: [
+                        "http://data.vlaanderen.be/ns/mandaat#Mandataris",
+                        "http://www.w3.org/ns/person#Person",
+                        "http://data.vlaanderen.be/ns/mandaat#TijdsgebondenEntiteit",
+                        "http://data.vlaanderen.be/ns/mandaat#Fractie",
+                        "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid",
+                        "http://www.w3.org/ns/prov#Location",
+                        "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+                        "http://data.vlaanderen.be/ns/persoon#Geboorte",
+                        "http://www.w3.org/ns/org#Membership",
+                        "http://data.vlaanderen.be/ns/mandaat#Mandaat",
+                        "http://mu.semte.ch/vocabularies/ext/BestuursfunctieCode",
+                        "http://mu.semte.ch/vocabularies/ext/MandatarisStatusCode",
+                        "http://mu.semte.ch/vocabularies/ext/BeleidsdomeinCode",
+                        "http://mu.semte.ch/vocabularies/ext/GeslachtCode",
+                        "http://mu.semte.ch/vocabularies/ext/BestuurseenheidClassificatieCode",
+                        "http://mu.semte.ch/vocabularies/ext/BestuursorgaanClassificatieCode",
+                        "http://www.w3.org/ns/org#Organization",
+                        "http://schema.org/PostalAddress",
+                        "http://www.w3.org/ns/org#Role",
+                        "http://www.w3.org/ns/org#Site",
+                        "http://schema.org/ContactPoint",
+                        "http://www.w3.org/ns/locn#Address",
+                        "http://data.lblod.info/vocabularies/leidinggevenden/Bestuursfunctie",
+                        "http://data.lblod.info/vocabularies/leidinggevenden/Functionaris",
+                        "http://data.lblod.info/vocabularies/leidinggevenden/FunctionarisStatusCode",
+                        "http://www.w3.org/2004/02/skos/core#Concept",
+                        "http://www.w3.org/2004/02/skos/core#ConceptScheme"
+                      ]
+                    } } ] }
+    ]
+  end
+end
+
+
+
