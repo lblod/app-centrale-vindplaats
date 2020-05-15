877135ccb38037672faa1539ed27fc846782d69a
Dries Hugaerts <dries.hugaerts@live.be>
2020-05-15 09:23:40 +0200

    added delta, remote-url-download & file services to the stack.

---
+++ config/delta/rule.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    match: {
+      subject: { }
+    },
+    callback: {
+      url: "http://resource/.mu/delta", method: "POST"
+    },
+    options: {
+      resourceFormat: "v0.0.0-genesis",
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://lblod.data.gift/file-download-statuses/ready-to-be-cached'
+      }
+    },
+    callback: {
+      method: 'POST',
+      url: 'http://download-url/process-remote-data-objects',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  }
+]
