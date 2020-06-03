d919017624fbecd70d2c9b55d207c00e3f2d8d27
Dries Hugaerts <dries.hugaerts@live.be>
2020-06-03 11:11:13 +0200

    verions bump "harvesting import" service + added classes to auth config.

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -15,13 +15,16 @@ defmodule Acl.UserGroups.Config do
                     graph: "http://mu.semte.ch/application",
                     constraint: %ResourceConstraint{
                       resource_types: [
+                        "http://data.vlaanderen.be/ns/mandaat#Verkiezingsresultaat",
+                        "http://data.vlaanderen.be/ns/mandaat#RechtstreekseVerkiezing",
+                        "http://data.vlaanderen.be/ns/mandaat#RechtsgrondBeeindiging",
+                        "http://data.vlaanderen.be/ns/mandaat#RechtsgrondAanstelling",
                         "http://data.vlaanderen.be/ns/mandaat#Mandataris",
                         "http://www.w3.org/ns/person#Person",
                         "http://data.vlaanderen.be/ns/mandaat#TijdsgebondenEntiteit",
                         "http://data.vlaanderen.be/ns/mandaat#Fractie",
-                        "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid",
+                        "http://data.vlaanderen.be/ns/mandaat#Kandidatenlijst",
                         "http://www.w3.org/ns/prov#Location",
-                        "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
                         "http://data.vlaanderen.be/ns/persoon#Geboorte",
                         "http://www.w3.org/ns/org#Membership",
                         "http://data.vlaanderen.be/ns/mandaat#Mandaat",
@@ -41,7 +44,20 @@ defmodule Acl.UserGroups.Config do
                         "http://data.lblod.info/vocabularies/leidinggevenden/Functionaris",
                         "http://data.lblod.info/vocabularies/leidinggevenden/FunctionarisStatusCode",
                         "http://www.w3.org/2004/02/skos/core#Concept",
-                        "http://www.w3.org/2004/02/skos/core#ConceptScheme"
+                        "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+                        "http://data.vlaanderen.be/ns/besluit#Agendapunt",
+                        "http://data.vlaanderen.be/ns/besluit#Artikel",
+                        "http://data.vlaanderen.be/ns/besluit#BehandelingVanAgendapunt",
+                        "http://data.vlaanderen.be/ns/besluit#Besluit",
+                        "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid",
+                        "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+                        "http://data.vlaanderen.be/ns/besluit#Stemming",
+                        "http://data.vlaanderen.be/ns/besluit#Vergaderactiviteit",
+                        "http://data.vlaanderen.be/ns/besluit#Zitting",
+                        "http://publications.europa.eu/resource/distribution/eli/owl/owl/eli.owl/#LegalResource",
+                        "http://publications.europa.eu/resource/distribution/eli/owl/owl/eli.owl/#LegalResourceSubdivision",
+                        "http://data.vlaanderen.be/ns/generiek#Versie",
+                        "http://data.vlaanderen.be/ns/generiek#VersieVolgensGeldigeTijd"
                       ]
                     } } ] }
     ]
