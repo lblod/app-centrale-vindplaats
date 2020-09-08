aa0330c245b64fd0e371aececbbb2308b95a42a0
achraf <achrafatauil@hotmail.com>
2020-09-08 17:23:37 +0200

    Position frontend-endpoint after cache

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -26,10 +26,6 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://frontend/assets/"
   end
 
-  match "/*path", @html do
-    Proxy.forward conn, path, "http://frontend/"
-  end
-
   match "/preflabel-discovery/api/v1/label/*path", @any do
     forward conn, path, "http://preflabel.org/api/v1/label/*path"
   end
@@ -40,16 +36,6 @@ defmodule Dispatcher do
 
   match "/uri-info/*path", @any do
     forward conn, path, "http://uri-info/"
-  end
-
-  match "/*_path", @html do
-    # *_path allows a path to be supplied, but will not yield
-    # an error that we don't use the path variable.
-    forward conn, [], "http://frontend/index.html"
-  end
-
-  match "/sparql", @any do
-    forward conn, [], "http://database:8890/sparql"
   end
 
   ###############
@@ -77,6 +63,20 @@ defmodule Dispatcher do
 
   match "_", %{ last_call: true, accept: %{ any: true } } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  end
+
+  match "/*path", @html do
+    Proxy.forward conn, path, "http://frontend/"
+  end
+
+  match "/*_path", @html do
+    # *_path allows a path to be supplied, but will not yield
+    # an error that we don't use the path variable.
+    forward conn, [], "http://frontend/index.html"
+  end
+  
+  match "/sparql", @any do
+    forward conn, [], "http://database:8890/sparql"
   end
 
   last_match
