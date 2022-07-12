14b4141b097202ef3ff9500b4a40f0df47a0e756
felix <ruiz.felixruiz@gmail.com>
2022-07-12 16:15:18 +0200

    Update config

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -66,6 +66,7 @@ defmodule Acl.UserGroups.Config do
                         "http://publications.europa.eu/resource/distribution/eli/owl/owl/eli.owl/#LegalResourceSubdivision",
                         "http://data.vlaanderen.be/ns/generiek#Versie",
                         "http://data.vlaanderen.be/ns/generiek#VersieVolgensGeldigeTijd",
+                        "http://lblod.data.gift/vocabularies/notification/NotificationRule"
                       ]
                     } } ] },
 
